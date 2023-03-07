3f1fcc4d202243c7d9991003899f8297ac48f041
sampadapachore <127177881+sampadapachore@users.noreply.github.com>
2023-03-07 21:04:59 +0530

    1 file added
    commit

---
+++ seleniumdemo/src/com/seleniumdemo/firstselenium.java
@@ -0,0 +1,10 @@
+package com.seleniumdemo;
+
+public class firstselenium {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+		//driver.get("file:///C:/Users/DELL/OneDrive/Documents/cascade.html");
+	}
+
+}
